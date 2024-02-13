0e27f843ccd10544ebc3a85e99d2d01a13e040fa
nitin sehgal <nitin.shegal@mobileprogramming.com>
2024-02-13 11:27:19 +0530

    first push

---
+++ index.js
@@ -0,0 +1,12 @@
+let express = require('express') ;
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
